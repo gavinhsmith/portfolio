f67752542c84691c157c7e47eb8531056645ac93
Gavin Smith <me@gavinhsmith.com>
2025-10-02 01:52:15 -0500

    Remove uncessesary pb

--- app/components/nav/link.vue
+++ app/components/nav/link.vue
@@ -3,10 +3,7 @@ defineProps(["href"]);
 </script>
 
 <template>
-  <a
-    :href="href"
-    class="transition-colors pb-0.5 text-white hover:text-accent-light"
-  >
+  <a :href="href" class="transition-colors text-white hover:text-accent-light">
     <span><slot /></span>
   </a>
 </template>
