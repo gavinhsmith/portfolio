040f4f1f0a953085bcaade111e43d09e0f02b8a5
Gavin Smith <me@gavinhsmith.com>
2025-10-06 17:12:03 -0500

    Patch up icon

--- app/components/nav/bar.vue
+++ app/components/nav/bar.vue
@@ -2,7 +2,9 @@
   <div
     class="absolute flex flex-row justify-between items-center w-full h-16 px-4 md:px-16 bg-light text-dark dark:bg-dark dark:text-light text-lg md:text-2xl font-accent lowercase"
   >
-    <NavLogo />
+    <div class="flex flex-row justify-start items-center w-full h-full">
+      <NavLogo />
+    </div>
 
     <div
       class="flex flex-row justify-end gap-3 md:gap-8 items-center w-full h-full"
--- app/components/nav/logo.vue
+++ app/components/nav/logo.vue
@@ -1,18 +1,17 @@
 <template>
-  <a
-    class="flex flex-row justify-start items-center pr-4 lg:pr-0 md:w-full h-full gap-8"
-    href="/"
-  >
-    <img
-      class="inline-block dark:hidden object-contain h-28"
-      src="/logo/default.svg"
-      alt="g Icon"
-    />
-    <img
-      class="hidden dark:inline-block object-contain h-28"
-      src="/logo/dark.svg"
-      alt="g Icon (Dark)"
-    />
-    <span class="hidden lg:inline-block">Gavin Smith</span>
+  <a class="flex flex-row justify-start items-center h-full gap-8" href="/">
+    <div class="h-full py-1 lg:py-0 lg:scale-175 lg:ml-6">
+      <img
+        class="block dark:hidden h-full"
+        src="~/assets/icon/default.svg"
+        alt="g Icon"
+      />
+      <img
+        class="hidden dark:block h-full"
+        src="~/assets/icon/dark.svg"
+        alt="g Icon (Dark)"
+      />
+    </div>
+    <span class="hidden lg:block">Gavin Smith</span>
   </a>
 </template>
