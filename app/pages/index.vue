291c8c93b52800b90e2cbc4a80b1eb9b69dd2ba9
Gavin Smith <me@gavinhsmith.com>
2025-10-07 00:07:02 -0500

    [Fix] still too big

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-full px-18 py-17">
+  <div class="w-full h-full p-10 lg:px-18 lg:py-17">
     <!-- Pushes down the content to not be behind the navbar. -->
     <NavHeadBumper />
 
