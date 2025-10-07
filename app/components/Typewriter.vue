4402682175d51d8678fd487732ebb72023b356c1
Gavin Smith <me@gavinhsmith.com>
2025-10-06 19:55:34 -0500

    Fixed typewriter to only run on client side

--- app/components/Typewriter.vue
+++ app/components/Typewriter.vue
@@ -34,62 +34,65 @@ const immediate = props.immediate ?? false;
 let currentWord = 0;
 let currentChar = 0;
 
-// Blinking carot.
-const carotInt = setInterval(() => {
-  carot.value = !carot.value;
-}, 500);
+// Only run this on the client, does not need to be server-side rendered.
+onMounted(() => {
+  // Blinking carot.
+  const carotInt = setInterval(() => {
+    carot.value = !carot.value;
+  }, 500);
 
-// Recursive function to create and destroy intervals, imitating typing.
-function createWordInterval() {
-  const interval = setInterval(() => {
-    // Add the next character of the word to the text, and increase the current char index.
-    value.value += words[currentWord].slice(currentChar, currentChar + 1);
-    currentChar++;
+  // Recursive function to create and destroy intervals, imitating typing.
+  function createWordInterval() {
+    const interval = setInterval(() => {
+      // Add the next character of the word to the text, and increase the current char index.
+      value.value += words[currentWord].slice(currentChar, currentChar + 1);
+      currentChar++;
 
-    // Check if we are at the end of the string.
-    if (currentChar == words[currentWord].length) {
-      // Clear the original adding interval.
-      clearInterval(interval);
+      // Check if we are at the end of the string.
+      if (currentChar == words[currentWord].length) {
+        // Clear the original adding interval.
+        clearInterval(interval);
 
-      // Reset char index and add 1 to word index.
-      currentChar = 0;
-      currentWord++;
+        // Reset char index and add 1 to word index.
+        currentChar = 0;
+        currentWord++;
 
-      // Check if that was the last word. If no looping, disable the carrot permanantly and we are done, else reset word index.
-      if (currentWord == words.length) {
-        if (!loop) {
-          clearInterval(carotInt);
-          carotOn.value = false;
-          return;
-        } // Breaks the loop, no intervals should remain at this time.
+        // Check if that was the last word. If no looping, disable the carrot permanantly and we are done, else reset word index.
+        if (currentWord == words.length) {
+          if (!loop) {
+            clearInterval(carotInt);
+            carotOn.value = false;
+            return;
+          } // Breaks the loop, no intervals should remain at this time.
 
-        currentWord = 0;
+          currentWord = 0;
+        }
+
+        // Set timeout to clear text after the waiting perioud.
+        setTimeout(() => {
+          // Clear text with "backspace."
+          const subintv = setInterval(() => {
+            // Slice away text.
+            value.value = value.value.slice(0, -1);
+
+            // When the string is empty again, clear "backspace" interval and start over with next word after waiting period.
+            if (value.value.length == 0) {
+              clearInterval(subintv);
+              setTimeout(createWordInterval, wait);
+            }
+          }, 50);
+        }, wait);
       }
+    }, 100);
+  }
 
-      // Set timeout to clear text after the waiting perioud.
-      setTimeout(() => {
-        // Clear text with "backspace."
-        const subintv = setInterval(() => {
-          // Slice away text.
-          value.value = value.value.slice(0, -1);
-
-          // When the string is empty again, clear "backspace" interval and start over with next word after waiting period.
-          if (value.value.length == 0) {
-            clearInterval(subintv);
-            setTimeout(createWordInterval, wait);
-          }
-        }, 50);
-      }, wait);
-    }
-  }, 150);
-}
-
-// Start inital typing.
-setTimeout(createWordInterval, immediate ? 0 : wait);
+  // Start inital typing.
+  setTimeout(createWordInterval, immediate ? 0 : wait);
+});
 </script>
 
 <template>
-  <div class="flex flex-row justify-start w-full">
+  <div class="flex flex-row justify-start">
     <span>{{ value }}</span>
     <!-- Set opacity of carot symbol based on carot value. Disable element if it is no longer needed. -->
     <span v-if="carotOn" :class="carot ? 'opacity-100' : 'opacity-0'">|</span>
--- app/components/nav/foot/bar.vue
+++ app/components/nav/foot/bar.vue
@@ -1,13 +1,29 @@
 <template>
   <div
-    class="flex flex-row justify-between items-center h-12 px-8 text-sm lg:text-lg font-accent bg-light text-dark dark:bg-dark dark:text-light"
+    class="flex flex-row justify-between items-center h-12 w-full px-4 lg:px-8 text-sm lg:text-lg font-accent bg-light text-dark dark:bg-dark dark:text-light"
   >
-    <div class="flex flex-row justify-start items-center">
+    <div class="w-1/2 lg:w-1/3 flex flex-row justify-start items-center">
       <span>(c) 2025 - Gavin Smith</span>
     </div>
-    <div class="flex flex-row justify-end items-center">
+    <div class="w-1/3 hidden lg:flex flex-row justify-center items-center">
+      <span
+        >Built with Love (and
+        <a
+          href="https://nuxt.com/"
+          target="_blank"
+          class="link hover:text-green-600"
+          >Nuxt</a
+        >)</span
+      >
+    </div>
+    <div class="w-1/2 lg:w-1/3 flex flex-row justify-end items-center">
       <Typewriter
-        :words="['Howdy!', 'Hello!', 'Hi!', 'Yo!']"
+        :words="[
+          'Never gonna give you up.',
+          'Never gonna let you down.',
+          'Never gonna run around.',
+          'And desert you!',
+        ]"
         :loop="true"
         :wait="750"
       />
