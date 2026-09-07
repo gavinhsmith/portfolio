48668affc35333b19dc915ee8505ffab73ecd12c
Gavin Smith <me@gavinhsmith.com>
2026-09-07 16:59:10 -0500

    [ui] Add displacement filter

---
+++ app/components/filter/Displacement.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg class="w-0 h-0 absolute">
+    <filter id="distort-filter">
+      <feTurbulence
+        type="turbulence"
+        baseFrequency="0.01"
+        numOctaves="3"
+        result="turbulence"
+      />
+      <feDisplacementMap
+        in="SourceGraphic"
+        in2="turbulence"
+        scale="35"
+        xChannelSelector="R"
+        yChannelSelector="G"
+      />
+    </filter>
+  </svg>
+
+  <div class="w-full h-full filter-[url(#distort-filter)]">
+    <slot />
+  </div>
+</template>
