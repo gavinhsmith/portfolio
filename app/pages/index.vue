4f387a2f1839d447b1bc01bd7d6a7a606cfded00
Gavin Smith <me@gavinhsmith.com>
2026-09-07 17:41:49 -0500

    [ui] Added displacement effect in bg of home, will add content later

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-    <div
-      class="w-full h-screen bg-ink-black flex flex-col items-center justify-center"
-    >
+    <div class="w-full h-screen absolute bg-ink-black overflow-hidden">
+      <FilterDisplacement> </FilterDisplacement>
+    </div>
+    <div class="w-full h-screen flex flex-col items-center justify-center">
       <div class="w-88">
         <ComputerAppContainer title="Welcome" icon="tabler:alert-hexagon">
           <div class="pl-10 pr-20 py-6 bg-pine-blue text-porcelain">
             <h1 class="mb-2 text-4xl text-light-cyan font-serif italic">
               Howdy!
             </h1>
-            <p>
+            <p class="text-lg">
               My name is Gavin Smith, and I am a Computer Science student at the
               University of Wisconsin-Madison.
             </p>
