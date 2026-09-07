6d7bb056e3a87f7c6718f4aa4aaee418422eb0d0
Gavin Smith <me@gavinhsmith.com>
2026-09-07 16:58:58 -0500

    [ui] Rename computer app window container

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
     <div
-      class="w-full h-screen bg-burnt-peach flex flex-col items-center justify-center"
+      class="w-full h-screen bg-ink-black flex flex-col items-center justify-center"
     >
       <div class="w-88">
-        <WindowLikeContainer title="Welcome" icon="tabler:alert-hexagon">
-          <div class="px-12 py-6 bg-ink-black text-porcelain">
-            <h1 class="text-4xl font-serif italic">Howdy!</h1>
-            <p>My name is Gavin</p>
+        <ComputerAppContainer title="Welcome" icon="tabler:alert-hexagon">
+          <div class="pl-10 pr-20 py-6 bg-pine-blue text-porcelain">
+            <h1 class="mb-2 text-4xl text-light-cyan font-serif italic">
+              Howdy!
+            </h1>
+            <p>
+              My name is Gavin Smith, and I am a Computer Science student at the
+              University of Wisconsin-Madison.
+            </p>
           </div>
-        </WindowLikeContainer>
+        </ComputerAppContainer>
       </div>
     </div>
   </div>
