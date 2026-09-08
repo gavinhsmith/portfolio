fff777d35ace29c05a2ba97cc5851a9ff35c8282
Gavin Smith <me@gavinhsmith.com>
2026-09-07 19:32:20 -0500

    [ui] Added new windows

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,10 +7,30 @@
       </FilterDisplacement>
     </div>
     <div class="w-full h-screen flex flex-col items-center justify-center">
-      <div class="w-88 lg:w-lg lg:mr-72">
-        <ComputerAppContainer title="Welcome" icon="tabler:alert-hexagon">
+      <div class="w-80 h-64 lg:h-80 absolute mt-136 lg:mt-96 ml-32 lg:ml-164">
+        <ComputerAppContainer title="Playing Now" icon="tabler:file-music">
+          <div
+            class="w-full h-full flex flex-col items-center justify-center bg-ink-black text-porcelain text-3xl font-mono"
+          >
+            W.I.P. :P
+          </div>
+        </ComputerAppContainer>
+      </div>
+
+      <div class="w-80 lg:w-lg h-64 absolute mb-136 lg:mb-96 mr-32 lg:ml-98">
+        <ComputerAppContainer title="Socials" icon="tabler:social">
+          <div
+            class="w-full h-full flex flex-col items-center justify-center bg-ink-black text-porcelain text-3xl font-mono"
+          >
+            W.I.P. :P
+          </div>
+        </ComputerAppContainer>
+      </div>
+
+      <div class="w-88 lg:w-xl lg:mr-88 absolute">
+        <ComputerAppContainer title="Welcome" icon="tabler:home-2">
           <div class="pl-10 pr-20 py-6 bg-honeydew text-ink-black">
-            <h1 class="text-4xl lg:text-5xl text-light-cyan font-serif italic">
+            <h1 class="text-4xl lg:text-5xl font-serif font-bold italic">
               Howdy!
             </h1>
             <div class="w-12 h-0.75 mb-6 mt-2 lg:mb-12 flex flex-row">
@@ -18,7 +38,7 @@
               <div class="bg-harvest-orange w-full h-full" />
               <div class="bg-oxblood w-full h-full" />
             </div>
-            <p class="text-lg">
+            <p class="lg:text-lg">
               My name is Gavin Smith, and I am a Computer Science student at the
               University of Wisconsin-Madison.
             </p>
